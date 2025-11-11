0fa40f1bb469891d8bcfa6d0ba5e5f9603db538e
Thormes Filgueira Leite Pereira <thormes.pereira@aluno.ufca.edu.br>
2025-11-11 11:21:39 -0300

    Update main.c
    Alterações realizadas em aula

--- Aula-02/main.c
+++ Aula-02/main.c
@@ -12,16 +12,20 @@ int main(){
     scanf("%d", &coluna);
     matriz = cria(linha, coluna);
     printf("Matriz foi criada com %d linhas e %d colunas\n", linhas(matriz), colunas(matriz));
-    printf("Digite um número para preencher a matriz inteira com esse valor: \n");
-    scanf("%f", &valor);
+    preenche(matriz);
     print_matriz(matriz);
-    printf("Digite o número da linha e da coluna para adicionar um valor:\n");
+    printf("Digite o numero da linha e da coluna para adicionar um valor (index 0):\n");
     scanf("%d %d", &linha, &coluna);
-    printf("Digite um valor numérico para armazenar na posição na matriz\n");
+    while(linha < 0 || coluna < 0){
+        printf("Linha e coluna devem ser maior que 0. Digite novamente:\n");
+        scanf("%d %d", &linha, &coluna);
+    }
+    printf("Digite um valor numerico para armazenar na posicao na matriz\n");
     scanf("%f", &valor);
     atribui(matriz, linha, coluna, valor);
     valor_interno = acessa(matriz, linha, coluna);
-    printf("O valor armazenado na matriz na linha %d e coluna %d é: %f\n", linha, coluna, valor_interno);
+    printf("O valor armazenado na matriz na linha %d e coluna %d eh: %f\n", linha, coluna, valor_interno);
     print_matriz(matriz);
+    printf("Liberando memoria\n");
     libera(matriz);
 }
